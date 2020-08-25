f987f5fef56e714ac9c72f6115aa622f65e7e1b8
imldy <im.ldy@outlook.com>
2020-08-26 02:28:16 +0800

    增加对http.client.RemoteDisconnected异常的处理

--- main.py
+++ main.py
@@ -4,6 +4,7 @@
 # 著作权归作者所有。商业转载请联系作者获得授权，非商业转载请注明出处。
 
 import _thread
+import http
 import requests
 import json
 import urllib
@@ -25,7 +26,7 @@ def down_image(Threadname, video):
         urllib.request.urlretrieve(full_url, full_file_name)
         print("{} Downloading image No.{} {}".format(Threadname, x, file_name))
         x += 1
-    except urllib.error.ContentTooShortError:
+    except urllib.error.ContentTooShortError or http.client.RemoteDisconnected:
         print('Network conditions is not good. Reloading...')
         down_image(Threadname, video)
 
