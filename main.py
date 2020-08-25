296ad7a1e3b558a881dcf37a7a248564d0e9f93e
imldy <im.ldy@outlook.com>
2020-08-26 01:31:03 +0800

    多线程，速度更快

--- main.py
+++ main.py
@@ -3,11 +3,22 @@
 # 来源：知乎
 # 著作权归作者所有。商业转载请联系作者获得授权，非商业转载请注明出处。
 
+import _thread
 import requests
 import json
 import urllib
+import random
 
 x = 1
+
+
+def down_image(Threadname, video):
+    global x
+    file_name = video['title'].replace("\\", "").replace("/", "").replace("r", "").replace(":", "").replace("*", "") \
+        .replace("?", "").replace("\"", "").replace("<", "").replace(">", "").replace("|", "")
+    urllib.request.urlretrieve("http:" + video['pic'], 'image-Thread/%s.jpg' % file_name)
+    print("{} Downloading image No.{} {}".format(Threadname, x, file_name))
+    x += 1
 
 
 def get_images(url):
@@ -15,12 +26,11 @@ def get_images(url):
         'Usar-Agent': 'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/72.0.3626.109 Safari/537.36'}
     res = requests.get(url, headers=headers, timeout=(0.5, 1))
     for video in res.json()['data']['list']['vlist']:
-        global x
-        file_name = video['title'].replace("\\", "").replace("/", "").replace("r", "").replace(":", "").replace("*", "") \
-            .replace("?", "").replace("\"", "").replace("<", "").replace(">", "").replace("|", "")
-        urllib.request.urlretrieve("http:" + video['pic'], 'image/%s.jpg' % file_name)
-        print("Downloading image No.{} {}".format(x, file_name))
-        x += 1
+        # 创建两个线程
+        try:
+            _thread.start_new_thread(down_image, ("new-Thread", video,))
+        except:
+            print("Error: 无法启动线程")
 
 
 def go(UP_UID, page_range_min, page_range_max):
@@ -36,3 +46,5 @@ if __name__ == '__main__':
     page_range_min = 1  # 查看UP主投稿视频页数，确定最大限制页码
     page_range_max = 10  # 查看UP主投稿视频页数，确定最大限制页码
     go(UP_UID, page_range_min, page_range_max)
+    while 1:
+        pass
